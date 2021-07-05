2a84c0c4d8fb15d1fa28e81933a224eafcd466b5
LimYunhwi <88yhtserof@naver.com>
2021-07-05 23:35:18 +0900

    2. 스위프트 처음 시작하기 2.1 ~ 2.2.2
    1. '예제 2-1 문자열 보간법 사용' 따라하기
    2. dump() 함수 사용해보기

---
+++ Chapter2.playground/Contents.swift
@@ -0,0 +1,12 @@
+/*
+ 2-1 문자열 보간법 사용
+ 문자열 보간법 String Interpolation
+ : 변수 또는 상수 등의 값을 문자열 내에 나타내고 싶을때 사용
+ */
+
+import UIKit
+
+let word:String="SWIFT"
+print("Hello, \(word)")
+dump(word)
+//dump()는 출력하려는 인스턴스의 자세한 내부 콘텐츠까지 출력한다.

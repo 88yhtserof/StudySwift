37b1a1360ce2b42ed54b17c42f9dfe6d90a83f97
LimYunhwi <88yhtserof@naver.com>
2021-07-30 23:56:25 +0900

    5 연산자 0 ~ 1.3
    1. 연산자 기본 설명 - NOTE_띄어쓰기와 연산자
    2. NOTE_스위프트의 나머지 연산과 나누기 연산자 나머지 공부 완료
    3. 비교 연산자 - NOTE_참조 비교 연산자

--- Chapter5.playground/Contents.swift
+++ Chapter5.playground/Contents.swift
@@ -1,5 +1,10 @@
 import UIKit
 
+/*
+ 띄어쓰기와 연산자
+ 스위프트에서는 띄어쓰기도 중요한 문법 중 하나. 어디에 공백이 있는지도 중요하다.
+ */
+
 //산술 연산자
 let value1: Int = 2
 let value2: Int = 10
@@ -17,5 +22,37 @@ print(value2%value3)  //나머지 반환
  스위프트는 데이터 타입에 굉장히 엄격하므로 서로 다른 자료형끼리의 연산을 엄격히 제한한다. Int와 Double 연산 안 됨
  */
 let number: Double = 5.0
-var result: Double = number.truncatingRemainder(dividingBy: 1.5) //나머지를 반환
-print(result)
+var result1: Double = number.truncatingRemainder(dividingBy: 1.5) //나머지를 반환
+print(result1)
+/*
+ result = number % 1.5 -> 에러
+ 부동 소수점의 경우 '%' 연산자 대신 truncatingRemainder을 사용한다.
+ */
+
+let result2 = number / 1
+
+print("결과 값: \(result2), 타입: \(type(of: result2))")
+
+//비교 연산자
+/*
+ A === B 참조가 같다.
+ A !== B 참조가 같지 않다.
+ A ~= B 패턴 매치되는지 확인하여 불리언 값 반환
+ 
+ 스위프트의 유일한 참조 타입인 클래스의 인스턴스에서만 참조 비교 연산자를 사용할 수 있다.
+ */
+let valueA: Int = 3
+let valueB: Int = 5
+let valueC: Int = 5
+
+let isSameValueAB: Bool = valueA == valueB //false
+let isSameValueBC: Bool = valueB == valueC //true
+
+/*
+let referenceA: SomeClass  = SomeClass()
+let referenceA: SomeClass  = SomeClass()
+let referenceA: SomeClass  = referenceA
+ 
+let isSameReferenceAB: Bool = referenceA === referenceB //false
+let isSameReferenceAC: Bool = referenceA === referenceC //true
+*/

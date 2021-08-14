35944ac53db1804a5a6c9dd1762d8f459b3e2a78
LimYunhwi <88yhtserof@naver.com>
2021-08-14 15:24:09 +0900

    7 함수 2.2
    1. 매개변수
    2. 예제 7-2
    3. 예제 7-3
    4. 예제 7-4
    5. 예제 7-5
    6. 예제 7-6
    7. 예제 7-7
    8. 예제 7-8

--- Chapter7.playground/Contents.swift
+++ Chapter7.playground/Contents.swift
@@ -30,3 +30,107 @@ func introduce(name: String) -> String {
 
 let introduceJenny: String = introduce(name: "Jenny")
 print(introduceJenny)
+
+
+//7.2.2 매개변수
+
+//7-2 매개변수가 없는 함수 정의와 사용
+func helloWorld() -> String {
+    return "Hello, world!"
+}
+print(helloWorld())
+
+//7-3 매개변수가 여러 개인 함수의 정의와 사용
+func sayHello(myName: String, yourName: String) -> String {
+    return "Hello \(yourName)! I'm \(myName)"
+}
+
+print(sayHello(myName: "88yhtserof", yourName: "Jenny"))
+
+//7-4 매개변수 이름과 전달인자 레이블을 가지는 함수 정의와 사용
+func sayHello(from myName: String, to name: String) -> String {
+    return "Hello \(name)! I'm \(myName)"
+}
+
+print(sayHello(from: "88yhtserof", to: "John"))
+
+//7-5 전달 인자 레이블이 없는 함수 정의와 사용
+func sayHello(_ name: String, _ times: Int) -> String {
+    var result: String = ""
+    
+    for _ in 0..<times {
+        result += "Hello "
+    }
+    
+    return result + name + "!"
+}
+
+print(sayHello("JoJo", 3))
+
+/*
+ 7-6 전달인자 레이블 변경을 통한 함수 중복 정의
+ 전달인자 레이블을 변경하면 함수의 이름 자체가 변경된다.
+ 그렇기 때문에 전달인자 에이블만 다르게 써주더라도 함수 중복 정의(오버로드)로 동작할 수 있다.
+ */
+func sayHello(to name: String, _ times: Int) -> String {
+    var result: String = ""
+    
+    for _ in 0..<times {
+        result += "Hello \(name)!" + " "
+    }
+    
+    return result
+}
+
+func sayHello(to name: String, repeatCount times: Int) -> String {
+    var result: String = ""
+    
+    for _ in 0..<times {
+        result += "Hello \(name)!" + " "
+    }
+    
+    return result
+}
+
+print(sayHello(to: "JOJO", 2))
+print(sayHello(to: "John", repeatCount: 4))
+
+//7-7 매개변수 기본 값이 있는 함수의 정의와 사용
+/*
+ 스위프트 함수에서는 매개변수마자 기본 값을 지정할 수 있다.
+ 매개변수 기본값이 있는 함수는 함수를 중복 정의한 것처럼 사용할 수 있다. 즉, 하나의 함수로 한 파라미터만 가진 함수, 두 개의 파라미터를 가진 함수를 사용할 수 있다.
+ 주로 기본 값이 없는 매개변수가 앞에 위치한다. 기본값이 없는 매개변수는 대체로 중요한 값을 전달할 가능성이 높다. 무엇보다도 기본값이 있는지와 상관없이 중요한 매개변수는 앞쪽에 위치하는 것이 좋다.
+ */
+func sayHello(_ name: String, times: Int = 2) -> String {
+    var result: String = ""
+    
+    for _ in 0..<times {
+        result += "Hello \(name)!" + "ye "
+    }
+    
+    return result
+}
+
+print(sayHello("JOO", times: 3))
+print(sayHello("KOO"))
+
+/*
+ 7-8 가변 매개변수를 가지는 함수의 정의와 사용
+ 매개변수로 몇 개의 값이 들어올지 모를 때 가변 매개변수를 사용할 수 있다.
+ 0개 이상의 값을 받아올 수 있으며, 가변 매개변수로 들어온 인자 값은 배열처럼 사용 가능
+ 1 함수 1 가변 매개변수
+ */
+func sayHelloToFriends(me: String, friends names: String... ) -> String {
+    var result: String = ""
+    
+    for friend in names{
+        result += "Hello \(friend)! "
+    }
+    
+    result += "I'm "+me + "!"
+    
+    return result
+}
+
+print(sayHelloToFriends(me: "88yhtserof")) //가변 매개변수는 0개 이상의 값을 받아올 수 있다.
+print(sayHelloToFriends(me: "88yhtserof", friends: "Jay", "John", "Harry"))

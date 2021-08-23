dea8ece88b91ef5795c4f054667c44b00d8daec3
LimYunhwi <88yhtserof@naver.com>
2021-08-24 00:32:21 +0900

    9 구조체와 클래스 2
    1. 클래스의 정의
    2. 예제 9-3 Person 클래스의 정의
    3. 클래스 인스턴스의 생성과 초기화
    4. 예제 9-4 Person 클래스의 인스턴스 생성 및 사용
    5. 클래스 인스턴스의 소멸
    6. 예제 9-5 클래스의 인스턴스 생성 및 소멸

--- Chapter9.playground/Contents.swift
+++ Chapter9.playground/Contents.swift
@@ -28,3 +28,51 @@ struct BasicInfomation {
 var yhInfo: BasicInfomation = BasicInfomation(name: "yh", age: 100)
 yhInfo.name = "hy" //var로 선언되는 경우 값을 변경해줄 수 있다.
 print("내 이름은 \(yhInfo.name)")
+
+//9.2 클래스
+//9.2.1 클래스의 정의
+/*
+ class 키워드 사용.
+ 클래스를 정의한다는 것은 새로운 타입을 생성해주는 것과 같다.
+ 클래스를 정의하는 방법은 구조체와 흡사. 다만 클래스는 상속 관계 가능
+ */
+class Person {
+    var height: Float = 0.0
+    var weight: Float = 0.0
+}
+
+/*
+ 9.2.2 클래스 인스턴스의 생성과 초기화
+ 구조체와 다르게 클래스의 인스턴스는 참조 타입이므로 클래스의 인스턴스를 상수 let 으로 선언해도 내부 프로퍼티 값을 변경 가능
+ */
+//9-4 Person 클래스의 인스턴스 생성 및 사요
+var myInfo: Person = Person() //클래스 인스턴스 생성
+myInfo.height = 200.3
+myInfo.weight = 100.2
+print("나의 키는 \(myInfo.height)이고, 나의 몸무게는 \(myInfo.weight)이다.")
+
+let jenny: Person = Person()
+jenny.height = 0.1
+jenny.weight = 0.01
+print("jenny의 키는 \(jenny.height)이고, jenny의 몸무게는 \(jenny.weight)이다.")
+
+
+/*
+ 9.2.3 클래스 인스턴스의 소멸
+ 클래스의 인스턴스는 참조 타입이므로 더는 참조할 필요가 없을 때 메로메리에서 해제된다. 이 과정을 소멸이라 한다.
+ 클래스 내부에 deinit 메서드를 구현해주면 소멸되기 직전 deinit라는 메서드가 호출된다.
+ deinit 메서드를 디이니셜라이저라고 부른다.
+ deinit 메서드는 클래스당 하나. 매개변수와 반환값 소유 불가
+ */
+//9-5 Fruit 클래스의 인스턴스 생성 및 소멸
+class Fruit {
+    var color: String = ""
+    var name: String = ""
+    
+    deinit {
+        print("Fruit 클래스의 인스턴스가 소멸된다.")
+    }
+}
+
+var myFruit: Fruit? = Fruit()
+myFruit = nil //deinit 메서드 호출
